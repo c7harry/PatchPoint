7ed5b25778454ef956295b1e00958fe5f03f57c7
c7harry <76096139+c7harry@users.noreply.github.com>
2025-08-13 01:02:09 -0700

    bug fixes

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,3 @@
-// Metro configuration for React Native
-// https://facebook.github.io/metro/docs/configuration
-
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
